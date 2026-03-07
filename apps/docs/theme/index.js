d3c046d8afe86dd7ebad106103abc916184d407a
thomasquan605 <thomas.quan605@gmail.com>
2026-03-07 23:30:47 +0800

    修改 vitepress 主题引用

--- apps/docs/theme/index.js
+++ apps/docs/theme/index.js
@@ -2,4 +2,9 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme, // 继承默认主题的所有 UI
+  enhanceApp({ app }) {
+    // 这里可以注册全局组件，目前留空即可
+  }
+}
